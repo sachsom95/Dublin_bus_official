2addef65495cbb9687591e96ae53dde34616be64
sachsom95 <sachin.soman@ucdconnect.ie>
2020-06-21 11:02:03 +0100

    Made the login page connect with a softlink added login to bus/urls and bus/views

--- django_server/dublin_bus/bus/urls.py
+++ django_server/dublin_bus/bus/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('login/',views.login,name="login")
+
 ]
--- django_server/dublin_bus/bus/views.py
+++ django_server/dublin_bus/bus/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'bus/index.html')
 
+def login(request):
+    return render(request,'bus/login.html')
