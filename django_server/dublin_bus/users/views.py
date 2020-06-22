ad12d9b00cf551e806b566c1504e2fd0d16e912a
sachsom95 <sachin.soman@ucdconnect.ie>
2020-06-22 21:02:31 +0100

    made the basic register function in views.py This function creates an instance of form and renders register.html we will make that next

--- django_server/dublin_bus/users/views.py
+++ django_server/dublin_bus/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+# importing the UserCreation form
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,'users/register.html',{'form':form})
+    
