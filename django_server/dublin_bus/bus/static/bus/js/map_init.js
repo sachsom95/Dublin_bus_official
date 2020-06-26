2e77d9f0de1a3b1bf438055b896173edf411821c
sachsom95 <sachin.soman@ucdconnect.ie>
2020-06-26 20:24:22 +0100

    Optimised the Auto Suggestion  suggest places inside dublin and also added 4 hidden forms to index.htm to store lat lng

--- django_server/dublin_bus/bus/static/bus/js/map_init.js
+++ django_server/dublin_bus/bus/static/bus/js/map_init.js
@@ -1,20 +1,23 @@
+/*options contains the dublin co-ordiantes and zoom level when 
+starting a map*/
 var options = {
     zoom:13,
     center:{lat:53.3498,lng:-6.2603}
 }
-
+let map
 function initMap(){
     //Map options
-    var map = new google.maps.Map(document.getElementById('map'),options)
+    map = new google.maps.Map(document.getElementById('map'),options)
 
 }
 
 function addMarker(coords) {
     //Add marker
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: coords,
-        map: new google.maps.Map(document.getElementById('map'), options),
-        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-
+        // why create a new map object and cause reload?
+        // map: new google.maps.Map(document.getElementById('map'), options),
+        map:map
     });
 }
+
