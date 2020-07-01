671ba57074856128b331fae5a26993f90fd87839
sachsom95 <sachin.soman@ucdconnect.ie>
2020-07-01 15:01:59 +0100

    urls in Project added to comply with login and logout

--- django_server/dublin_bus/dublin_bus/urls.py
+++ django_server/dublin_bus/dublin_bus/urls.py
@@ -20,6 +20,8 @@ from django.urls import path,include
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 from users import views as user_views
+# login-logout forms
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -29,7 +31,8 @@ urlpatterns = [
     path('register/',user_views.register,name='register'),
     # path('login/', views.index,name="login")
     path('bus/',include('bus.urls')),
-    path('login/', views.index),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('account', user_views.account, name='account')
 
 ]
