6bfd04e87bd77b9ff94a84d5471f0de8c7825cc8
Ben <bencabe93@gmail.com>
2020-07-05 17:35:34 +0100

    added initial app prediction

--- django_server/dublin_bus/bus/static/bus/js/journey_time_prediction.js
+++ django_server/dublin_bus/bus/static/bus/js/journey_time_prediction.js
@@ -1,24 +1,50 @@
-let buses = {}
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    // var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay; 
+}
 
 $( "#submit" ).click(function() {
-    let x = 0;
+    // let x = 0;
+    var time_prediction = 0
     console.log(route)
     for (i=0; i<route[0]['steps'].length;i++){
         if (route[0]['steps'][i]['travel_mode'] == "TRANSIT"){
-            buses[x] = route[0]['steps'][i]['transit']['line']['short_name'];
-            console.log(route[0]['steps'][i]['transit']['line']['start_location']['lat'])
-            x += 1;
-        }   
-    }
-    // $.ajax({
-    //     url: '/prediction/',
-    //     type: 'GET',
-    //     data: buses,
-    //     success: function (data) {
-    //         alert(data.response);
-    //     }
+            var line = route[0]['steps'][i]['transit']['line']['short_name'];
+            var dep_lat = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['departure_stop']['location']))['lat']
+            var dep_lng = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['departure_stop']['location']))['lng']
+            // console.log(dep_lat,dep_lng)
+            var arr_lat = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['arrival_stop']['location']))['lat']
+            var arr_lng = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['arrival_stop']['location']))['lng']
+            // console.log(arr_lat,arr_lng)
+            var google_pred = route[0]['steps'][i]['duration']['value']
+            // x += 1;
 
-    // })
+            $.ajax({
+                url: '/prediction/',
+                type: 'GET',
+                async: false,
+                data: {'line': line,'dep_lat':dep_lat,'dep_lng':dep_lng,'arr_lat':arr_lat,'arr_lng':arr_lng,'google_pred':google_pred},
+                success: function (data) {
+                        time_prediction = parseInt(time_prediction) + parseInt(data.prediction)
+                        console.log(data.prediction)
+                }
+        
+            })
+        }
+    }
+    // alert('Your bus journey will take' + time_prediction + 'seconds')
+    // console.log(parseInt(time_prediction))
+    document.getElementById('precitionResult').innerHTML = "Your bus journey(s) will take: " + secondsToHms(time_prediction)
 
 });
+
+
 
