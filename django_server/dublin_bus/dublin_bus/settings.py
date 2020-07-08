d12781cae753639315ad1a6e653ac15b593871d6
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2020-07-08 18:42:22 +0100

    minor deletions

--- django_server/dublin_bus/dublin_bus/settings.py
+++ django_server/dublin_bus/dublin_bus/settings.py
@@ -80,17 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-'bus_weather': { # this is our  db, already created
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bus_weather',
-        'USER': 'root',
-        'PASSWORD': 'Intecitron22!',
-        'HOST': 'localhost',
-        'PORT': '3306',
     }
-
-}
 
 
 # Password validation
