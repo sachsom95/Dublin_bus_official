b30d1009e07244de6db021d6abc4428a4947df51
Ben <bencabe93@gmail.com>
2020-07-22 13:25:02 +0100

    changed display format for current weather model

--- django_server/dublin_bus/bus/models.py
+++ django_server/dublin_bus/bus/models.py
@@ -1,26 +1,65 @@
 from django.db import models
+from datetime import datetime 
 
 # Create your models here.
-class Currentweather(models.Model):
-    dt = models.IntegerField(primary_key=True)
-    dt_iso = models.DateTimeField()
-    city_id = models.IntegerField(blank=True, null=True)
-    city_name = models.CharField(max_length=45, blank=True, null=True)
-    lat = models.CharField(max_length=45, blank=True, null=True)
-    lon = models.CharField(max_length=45, blank=True, null=True)
-    temp = models.DecimalField(max_digits=6, decimal_places=3, blank=True, null=True)
-    temp_min = models.DecimalField(max_digits=6, decimal_places=3, blank=True, null=True)
-    temp_max = models.DecimalField(max_digits=6, decimal_places=3, blank=True, null=True)
-    pressure = models.CharField(max_length=45, blank=True, null=True)
-    humidity = models.CharField(max_length=45, blank=True, null=True)
-    wind_speed = models.CharField(max_length=45, blank=True, null=True)
-    wind_deg = models.CharField(max_length=45, blank=True, null=True)
-    clouds_all = models.IntegerField(blank=True, null=True)
-    weather_id = models.IntegerField(blank=True, null=True)
-    weather_main = models.CharField(max_length=45, blank=True, null=True)
-    weather_description = models.CharField(max_length=45, blank=True, null=True)
-    weather_icon = models.CharField(max_length=45, blank=True, null=True)
 
+class Currentweather(models.Model):
+    dt = models.CharField(max_length=45)
+    temp = models.CharField(max_length=45,null=True)
+    wind_speed = models.CharField(max_length=45,null=True)
+    wind_deg = models.CharField(max_length=45,null=True)
+    weather_id = models.CharField(max_length=45,null=True)
+    weather_main = models.CharField(max_length=45,null=True)
+    weather_description = models.CharField(max_length=45,null=True)
+    weather_icon = models.CharField(max_length=45,null=True)
+    def __str__(self):
+        return datetime.fromtimestamp(int(self.dt))
+
+    # def __str__(self):
+    #     return self.weather_icon
     # class Meta:
-    #     managed = False
     #     db_table = 'currentWeather'
+    #     app_label = "dublin_bus"
+
+# class forecastWeather(models.Model):
+#     dt = models.CharField(max_length=45,primary_key=True)
+#     dt_iso = models.DateField()
+#     timestamp = models.DateField()
+#     temp = models.CharField(max_length=45)
+#     temp_min = models.CharField(max_length=45)
+#     temp_max = models.CharField(max_length=45)
+#     pressure = models.CharField(max_length=45)
+#     humidity = models.CharField(max_length=45)
+#     wind_speed = models.CharField(max_length=45)
+#     wind_deg = models.CharField(max_length=45)
+#     clouds_all = models.CharField(max_length=45)
+#     weather_id = models.CharField(max_length=45)
+#     weather_main = models.CharField(max_length=45)
+#     weather_description = models.CharField(max_length=45)
+#     weather_icon = models.CharField(max_length=45)
+
+#     class Meta:
+#         db_table = 'forecastWeather'
+#         app_label = "dublin_bus"
+
+# class covid(models.Model):
+#     Date = models.CharField(max_length=45,primary_key=True)
+#     RequiringICUCovidCases = models.CharField(max_length=45)
+#     CommunityTransmission = models.CharField(max_length=45)
+#     TotalConfirmedCovidCases = models.CharField(max_length=45)
+#     TotalCovidDeaths = models.CharField(max_length=45)
+#     ConfirmedCovidCases = models.CharField(max_length=45)
+#     ConfirmedCovidDeaths = models.CharField(max_length=45)
+#     CloseContact = models.CharField(max_length=45)
+#     StatisticsProfileDate = models.CharField(max_length=45)
+#     FID = models.CharField(max_length=45)
+#     TravelAbroad = models.CharField(max_length=45)
+#     HospitalisedCovidCases = models.CharField(max_length=45)
+#     timestamp = models.DateField()
+#     # def __str__(self):
+#     #     return self.weather_icon
+
+#     class Meta:
+#         db_table = 'covid'
+#         app_label = "dublin_bus"
+
--- django_server/dublin_bus/cur_weather_update.py
+++ django_server/dublin_bus/cur_weather_update.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dublin_bus.settings")
 
@@ -7,10 +9,30 @@ django.setup()
 
 from bus.models import Currentweather
 
+# get current weather from openweathermap api
+apikey='409a981102cfc744586072ad35c0fb0f'
+cityid='2964574'
+
+url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityid + "&APPID=" + apikey
+
+response = requests.get(url)
+cur_weather = json.loads(response.text)
+
+# delete records in current weather model since we only ever want 1 record 
+Currentweather.objects.all().delete()
+
+# create an instance of current weather class
 w = Currentweather()
 
-w.dt = 908763
+# add variables to the instance from openweathermap data
+w.dt = cur_weather['dt']
+w.temp = cur_weather['main']['temp'] - 273.15
+w.wind_speed = cur_weather['wind']['speed']
+w.wind_deg = cur_weather['wind']['deg']
+w.weather_id = cur_weather['weather'][0]['id']
+w.weather_main = cur_weather['weather'][0]['main']
+w.weather_description = cur_weather['weather'][0]['description']
+w.weather_icon = cur_weather['weather'][0]['icon']
 
-w.dt_iso = '2020-07-15 14:20:19'
-
+# save new record
 w.save()
