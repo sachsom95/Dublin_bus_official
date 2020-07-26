65016caaec3ac4e9866aa36ef5ad4305739113a2
sachsom95 <sachin.soman@ucdconnect.ie>
2020-07-26 13:08:27 +0100

    AMost issues with call back cleared only bug left is the link

--- django_server/dublin_bus/bus/static/bus/js/journey_time_prediction.js
+++ django_server/dublin_bus/bus/static/bus/js/journey_time_prediction.js
@@ -11,8 +11,48 @@ function secondsToHms(d) {
     return hDisplay + mDisplay; 
 }
 
-$( "#submit-btn" ).click(function() {
-    // let x = 0;
+$( "#submit-btn" ).click(predict())
+
+// $( "#submit-btn" ).click(function() {
+//     // let x = 0;
+//     var time_prediction = 0
+//     console.log(route)
+//     for (i=0; i<route[0]['steps'].length;i++){
+//         if (route[0]['steps'][i]['travel_mode'] == "TRANSIT"){
+//             var line = route[0]['steps'][i]['transit']['line']['short_name'];
+//             var dep_lat = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['departure_stop']['location']))['lat']
+//             var dep_lng = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['departure_stop']['location']))['lng']
+//             // console.log(dep_lat,dep_lng)
+//             var arr_lat = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['arrival_stop']['location']))['lat']
+//             var arr_lng = JSON.parse(JSON.stringify(route[0]['steps'][i]['transit']['arrival_stop']['location']))['lng']
+//             // console.log(arr_lat,arr_lng)
+//             var google_pred = route[0]['steps'][i]['duration']['value']
+//             // x += 1;
+
+//             $.ajax({
+//                 url: '/prediction/',
+//                 type: 'GET',
+//                 async: false,
+//                 data: {'line': line,'dep_lat':dep_lat,'dep_lng':dep_lng,'arr_lat':arr_lat,'arr_lng':arr_lng,'google_pred':google_pred},
+//                 success: function (data) {
+//                         time_prediction = parseInt(time_prediction) + parseInt(data.prediction)
+//                         console.log(data.prediction)
+//                 }
+        
+//             })
+//         }
+//     }
+//     // alert('Your bus journey will take' + time_prediction + 'seconds')
+//     // console.log(parseInt(time_prediction))
+//     document.getElementById("precitionResult").innerHTML = "Your bus journey(s) will take: " + secondsToHms(time_prediction)
+//     // sachin: this here is for geting the sharable link
+//     document.getElementById("share_trip").style.visibility="visible"
+// });
+
+
+function predict(){
+
+
     var time_prediction = 0
     console.log(route)
     for (i=0; i<route[0]['steps'].length;i++){
@@ -45,7 +85,11 @@ $( "#submit-btn" ).click(function() {
     document.getElementById("precitionResult").innerHTML = "Your bus journey(s) will take: " + secondsToHms(time_prediction)
     // sachin: this here is for geting the sharable link
     document.getElementById("share_trip").style.visibility="visible"
-});
+
+
+}
+
+
 
 
 function get_shareable_link()
