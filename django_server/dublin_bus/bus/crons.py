5f9ea2a676c7a92a9baf2a13b83519a18e9e824a
Ben <bencabe93@gmail.com>
2020-07-30 14:19:05 +0100

    added json file for file paths

--- django_server/dublin_bus/bus/crons.py
+++ django_server/dublin_bus/bus/crons.py
@@ -2,15 +2,21 @@
 # -*- coding:utf-8 -*-
 
 import os
+import json
+
+with open(os.path.abspath('static/file_paths.json')) as config_file:
+    config = json.load(config_file)
+
 def currentweather():
     # f=open('/Users/chenpeng/PycharmProjects/comp_47360/Dublin_bus_official/django_server/dublin_bus/bus/test.txt','w')
     # f.writelines('hello')
     # f.close()
     # exec(open("Currentweather_update.py").read())
-    os.system("/opt/anaconda3/envs/comp47350py37/bin/python /Users/chenpeng/PycharmProjects/comp_47360/Dublin_bus_official/django_server/dublin_bus/bus/Currentweather_update.py")
+    os.system(config['python_path']+ ' ' + config['current_weather_file'])
+    # os.path.abspath('Dublin_bus_official/django_server/dublin_bus/bus/Currentweather_update.py')
 
 def forecastweather():
-    os.system("/opt/anaconda3/envs/comp47350py37/bin/python /Users/chenpeng/PycharmProjects/comp_47360/Dublin_bus_official/django_server/dublin_bus/bus/Forecasttweather_update.py")
+    os.system(config['python_path']+ ' ' + config['forecast_weather_file'])
 
 def covid():
-    os.system("/opt/anaconda3/envs/comp47350py37/bin/python /Users/chenpeng/PycharmProjects/comp_47360/Dublin_bus_official/django_server/dublin_bus/bus/Covid_update.py")
+    os.system(config['python_path']+ ' ' + config['covid_file'])
